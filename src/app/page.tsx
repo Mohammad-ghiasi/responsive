87cf6fa5a32c30a0b80dac542bf8cb35a9bc4bd3
mohammad <mohammadghiasi005@gamil.com>
2024-03-21 14:00:49 +0330

    responsive-app-v2-stleless

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,30 @@ import prfum from "../../public/asset/image/image-product-desktop.jpg"
 export default function Home() {
   return (
     <main>
-      <div className="w-100 h-[100vh] flex justify-center items-center bg-gray-300">
-        <div className="w-96  md:max-lg:w-[70%] lg:max-2xl:w-[61%] grid place-items-center grid-cols-1 md:max-2xl:grid-cols-2 gap-0 mx-3 rounded-xl overflow-hidden shadow-xl shadow-gray-500">
+      <div className="w-100 h-[100vh] flex justify-center items-center">
+        <div className="w-96  md:max-lg:w-[70%]  lg:max-2xl:w-[54%] grid  grid-cols-1 md:max-2xl:grid-cols-2 gap-0 mx-3 rounded-xl overflow-hidden shadow-xl shadow-gray-500">
 
           <div className="w-full overflow-hidden">
             <div className="transition-all  hover:scale-110">
               <Image
                 alt="perfum"
                 src={prfum}
+                priority={true}
+                blurDataURL=""
               />
             </div>
           </div>
 
-          <div className="md:max-2xl:ms-4 h-[100%]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta velit autem consequatur optio error vel recusandae ullam nam, eum voluptatem ab, dolorum doloribus doloremque repudiandae distinctio ut, reiciendis explicabo tenetur.
+          <div className="md:max-2xl:ms-4 h-[100%] flex  items-center py-6 px-4">
+            <div className="flex flex-col space-y-5 md:max-xl:space-y-10 xl:max-2xl:space-y-16">
+              <p className="tracking-[.5em]">PREFUME</p>
+              <h2>Gabrielle Essence Eau De Parfum</h2>
+              <p>A floral, solar and voluptuous interpretation composed by
+                Olivier Polge, Perfumer-Creator for the House of CHANEL.
+              </p>
+              <p>$149.99 <span><del>169.98</del></span></p>
+              <button className="bg-green-700">Add  to Cart</button>
+            </div>
           </div>
         </div>
       </div>
